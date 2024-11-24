985af90606e440ba0cc95077dd21578c49098513
Mohammed Ou-zaa <ouzaa.contact@gmail.com>
2024-11-24 12:43:46 +0100

    Add register page & register form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from "../components/register-form";
+
+export default function Page() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <RegisterForm />
+    </div>
+  );
+}
