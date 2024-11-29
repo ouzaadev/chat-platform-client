56fc58865a41a6115fa1e099d3681016fd387177
Mohammed Ou-zaa <ouzaa.contact@gmail.com>
2024-11-29 14:13:01 +0100

    Add next font (geist)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import { Geist } from "next/font/google";
 import "./globals.css";
+
+const geist = Geist({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ChatHub",
@@ -13,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={geist.className}>{children}</body>
     </html>
   );
 }
